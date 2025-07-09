f6842c40fd64f24014db7bfb69ee737fd9535428
Mike Bowie <mike.bowie@collectivehealth.com>
2025-07-09 16:50:57 -0700

    Update uart_component_esp_idf.cpp

--- components/uart/uart_component_esp_idf.cpp
+++ components/uart/uart_component_esp_idf.cpp
@@ -115,6 +115,8 @@ void IDFUARTComponent::setup() {
     this->mark_failed();
     return;
   }
+  // Not sure if this is required, however not seeing pin-shifting happening
+  uart_set_mode(this->uart_num_, UART_MODE_RS485_HALF_DUPLEX)
 
   uint32_t invert = 0;
   if (this->tx_pin_ != nullptr && this->tx_pin_->is_inverted())
