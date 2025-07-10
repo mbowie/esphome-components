887424499b0f61e7b1db1037451d5744dddf4363
Mike Bowie <mike.bowie@collectivehealth.com>
2025-07-09 23:26:05 -0700

    Update uart_component_esp_idf.cpp

--- components/uart/uart_component_esp_idf.cpp
+++ components/uart/uart_component_esp_idf.cpp
@@ -112,14 +112,14 @@ void IDFUARTComponent::setup() {
   int8_t rts = this->rts_pin_ != nullptr ? this->rts_pin_->get_pin() : UART_PIN_NO_CHANGE;
 
   // Hard-code the pull-down for the RTS pin
-  gpio_config_t rts_gpio_config = {
-    .pin_bit_mask = (1ULL << rts),
-    .mode = GPIO_MODE_OUTPUT,
-    .pull_up_en = GPIO_PULLUP_DISABLE,
-    .pull_down_en = GPIO_PULLDOWN_ENABLE,
-    .intr_type = GPIO_INTR_DISABLE,
-  };
-  gpio_config(&rts_gpio_config);
+  // gpio_config_t rts_gpio_config = {
+  //   .pin_bit_mask = (1ULL << rts),
+  //   .mode = GPIO_MODE_OUTPUT,
+  //   .pull_up_en = GPIO_PULLUP_DISABLE,
+  //   .pull_down_en = GPIO_PULLDOWN_ENABLE,
+  //   .intr_type = GPIO_INTR_DISABLE,
+  // };
+  // gpio_config(&rts_gpio_config);
   
   err = uart_set_pin(this->uart_num_, tx, rx, rts, cts);
   if (err != ESP_OK) {
@@ -128,8 +128,13 @@ void IDFUARTComponent::setup() {
     return;
   }
   // Not sure if this is required, however not seeing pin-shifting happening
-  uart_set_mode(this->uart_num_, UART_MODE_RS485_HALF_DUPLEX);
-  uart_set_rts(this->uart_num_, 1); // Set RTS high initially (active low RTS)
+  err = uart_set_mode(this->uart_num_, UART_MODE_RS485_HALF_DUPLEX);
+ if (err != ESP_OK) {
+    ESP_LOGW(TAG, "uart_set_mode failed: %s", esp_err_to_name(err));
+    this->mark_failed();
+    return;
+  }
+  //uart_set_rts(this->uart_num_, 0); // Set RTS high initially (active low RTS)
 
   uint32_t invert = 0;
   if (this->tx_pin_ != nullptr && this->tx_pin_->is_inverted())
